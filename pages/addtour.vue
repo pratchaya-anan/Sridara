2f2b78a6aae8dcb2f79d417f749ff9d1cb2e2570
Phachara1001 <bossyoso789@gmail.com>
2023-03-11 11:20:22 +0700

    Boss Modify AddTour

--- pages/addtour.vue
+++ pages/addtour.vue
@@ -27,6 +27,7 @@
             <input type="text" id="base-input"
               class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
           </div>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           <div class="mb-6">
             <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">เบอร์โทร</label>
             <input type="text" id="small-input"
@@ -45,6 +46,7 @@
             <input type="text" id="base-input"
               class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
           </div>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           <div class="mb-6">
             <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">จำนวนคืน</label>
             <input type="text" id="small-input"
@@ -81,7 +83,7 @@
               placeholder="เลือกวันที่">
           </div>
         </div>
-
+        <br>
         <div class="mb-6">
           <label for="base-input"
             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">จำนวนลูกทัวร์</label>
