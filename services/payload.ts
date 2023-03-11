f18874486cc2994292a9c7de8efbc9756df4e64a
Ai-Sasit <sasit2555@gmail.com>
2023-03-11 12:32:09 +0700

    add config

--- services/payload.ts
+++ services/payload.ts
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+
 export const group_members = (
   tour_id: string,
   thai_name: string,
@@ -21,6 +23,62 @@ export const group_members = (
       },
       sequences: {
         stringValue: sequences,
+      },
+    },
+  };
+};
+
+export const group_tours = (
+  id: string,
+  trip_name: string,
+  program_tour: string,
+  go_date: Date,
+  back_date: Date,
+  day: number,
+  night: number,
+  vehicle_income: string,
+  vehicle_outcome: string,
+  guide_name: Array<string>,
+  guide_tel: Array<string>,
+  amount_member: number
+) => {
+  return {
+    fields: {
+      id: {
+        stringValue: id,
+      },
+      trip_name: {
+        stringValue: trip_name,
+      },
+      program_tour: {
+        stringValue: program_tour,
+      },
+      go_date: {
+        stringValue: dayjs(go_date).format("DD/MM/YYYY"),
+      },
+      back_date: {
+        stringValue: dayjs(back_date).format("DD/MM/YYYY"),
+      },
+      day: {
+        stringValue: String(day),
+      },
+      night: {
+        stringValue: String(night),
+      },
+      vehicle_income: {
+        stringValue: vehicle_income,
+      },
+      vehicle_outcome: {
+        stringValue: vehicle_outcome,
+      },
+      guide_name: {
+        stringValue: guide_name.join(","),
+      },
+      guide_tel: {
+        stringValue: guide_tel.join(","),
+      },
+      amount_member: {
+        stringValue: String(amount_member),
       },
     },
   };
